a8522121b284b0cd60a627874d584bfcb2524f49
ashleigh137 <ashleigh137886@gmail.com>
2026-02-11 17:57:41 +1100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function change()
+{
+    document.getElementById("demo").innerHTML="Good evening Ashleigh";
+}
+var a=15, b=3;
+document.getElementById('sum').innerHTML= "sum=" + (a+b);
+document.getElementById('dif').innerHTML= "Difference=" + (a-b);
+document.getElementById('pro').innerHTML= "Product=" + (a*b);
+document.write("Data type example:" + "<br>");
+var x=5;
+document.write(typeof(x)+ "<br>");
+var x='hello';
+document.write(typeof(x)+ "<br>");
+var x=true;
+document.write(typeof(x)+ "<br>");
